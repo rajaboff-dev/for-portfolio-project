ff769e0e3a544a0725cf44de12d23d5b8d74ea47
rajaboff007 <asadbekrajaboff1337@gmail.com>
2024-09-26 22:19:50 +0500

    bablabl

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
+let bar = document.querySelector(".bar");
+let barActive = document.querySelector(".bar-active");
 
-let bar = document.querySelector('.bar');
-let barActive = document.querySelector('.bar-active');
-
-
-bar.addEventListener('click', () => {
-    bar.classList.toggle('bar-active')
-    document.querySelector('.nav-links').classList.toggle('nav-active')
-})
-
-
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(TextPlugin);
-    gsap.to(".main-text", {
-        duration: 1,
-        text: "Rajaboff",
-        ease: "none",
-    });
+bar.addEventListener("click", () => {
+  bar.classList.toggle("bar-active");
+  document.querySelector(".nav-links").classList.toggle("nav-active");
 });
 
-if(window.scrollY > 3){
-    document.querySelector('header').style.boxShadow = 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px';
-}else{
-    document.querySelector('header').style.boxShadow = 'none';
-}
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    document.querySelector("header").style.boxShadow =
+      "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px";
+  } else {
+    document.querySelector("header").style.boxShadow = "none";
+  }
+});
 
-document.getElementById('home').addEventListener('mousemove', (event) => {
-    let home = document.getElementById('home');
-    if(event.clientX < 300){
-        home.style.transform = 'translateX(10px)';
-    }else if(event.clientX > 1200){
-        home.style.transform = 'translateX(-10px)';
-    }else if(event.clientY < 300){
-        home.style.transform = 'translateY(10px)';
-    }else if(event.clientY > 600){
-        home.style.transform = 'translateY(-10px)';
-    }else{
-        home.style.transform = 'translate(0px, 0px)';
-    }
-    
-    
-})
+document.getElementById("home").addEventListener("mousemove", (event) => {
+  let home = document.getElementById("home");
+  if (event.clientX < 300) {
+    home.style.transform = "translateX(10px)";
+  } else if (event.clientX > 1200) {
+    home.style.transform = "translateX(-10px)";
+  } else if (event.clientY < 300) {
+    home.style.transform = "translateY(10px)";
+  } else if (event.clientY > 600) {
+    home.style.transform = "translateY(-10px)";
+  } else {
+    home.style.transform = "translate(0px, 0px)";
+  }
+});
+
+anime({
+  targets: ".main-text span",
+  translateY: -50,
+  delay: anime.stagger(100), // delay starts at 500ms then increase by 100ms for each elements.
+  loop: true,
+  direction: "alternate",
+});
